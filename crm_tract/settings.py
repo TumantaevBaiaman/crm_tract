67ca8806615adb5ac7c4d6a39654aea4a8ae6cf9
Tumantaev Baiaman <baiaman.tumantaev.kk@gmail.com>
2022-12-13 20:30:29 +0600

    settings corsheaders

--- crm_tract/settings.py
+++ crm_tract/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     # install
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # corsheaders
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://172.27.32.1:3000",
 ]
 
 ROOT_URLCONF = 'crm_tract.urls'
