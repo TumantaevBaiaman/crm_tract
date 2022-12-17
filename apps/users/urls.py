8d9c0e8ac4460a97398071d63820d311b895f746
Your Name <you@example.com>
2022-12-17 14:03:43 +0600

    status list

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import (
     ViewRegisterAccount,
     ViewRegisterUser, LoginView,
-    ViewListUser
+    ViewListUser, ViewListStatus
 )
 
 urlpatterns = [
@@ -11,5 +11,6 @@ urlpatterns = [
     path('register/user/', ViewRegisterUser.as_view(), name='register-user'),
     path('login/', LoginView.as_view(), name='token'),
     path('user_list/', ViewListUser.as_view(), name='employee-list'),
+    path('status_list/', ViewListStatus.as_view(), name='status-list'),
 
 ]
