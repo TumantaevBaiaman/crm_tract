b969da6573d7245f6401150f4d837ce603c4ccda
Your Name <you@example.com>
2023-01-04 13:00:49 +0600

    invoice get by id and list

--- apps/cars/serializers.py
+++ apps/cars/serializers.py
@@ -1,12 +1,6 @@
-from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
 
 from . import models
-from ..users.models import ModelsUser
-from apps.users.serializers import (
-    SerializerUser
-)
 
 
 class SerializerCar(serializers.ModelSerializer):
--- apps/invoice/serializers.py
+++ apps/invoice/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from . import models
+from ..cars.serializers import SerializerCar
+from ..customer.serializers import SerializerCustomer
+from ..task.serializers import SerializerTask
+from ..users.serializers import SerializerUser
 
 
 class SerializerInvoice(serializers.ModelSerializer):
+    car_id = SerializerCar(read_only=True)
+    customer_id = SerializerCustomer(read_only=True)
+    crew_id = SerializerUser(read_only=True)
+    tasks = SerializerTask(many=True, read_only=True)
 
     class Meta:
         model = models.ModelsInvoice
