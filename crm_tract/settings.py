f9c8e5ff098e0895a48f41eb7c360e6ff2ee4318
Your Name <you@example.com>
2023-02-19 20:10:41 +0600

    media

--- crm_tract/settings.py
+++ crm_tract/settings.py
@@ -173,12 +173,11 @@ EMAIL_HOST_PASSWORD = 'fzrdijrpfueypkoc'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 
 MEDIA_URL = '/media/'
-
-STATIC_URL = '/static/'
 STATIC_ROOT = (BASE_DIR / 'staticfiles')
+STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
