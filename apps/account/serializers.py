cda691605274a8ac49cb92b4c458c74b67241367
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2023-04-08 14:37:02 +0600

    fix

--- apps/account/serializers.py
+++ apps/account/serializers.py
@@ -2,7 +2,16 @@ from rest_framework import serializers
 from . import models
 
 
+class ModelsStatusSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.ModelsSatusAccount
+        fields = '__all__'
+
+
 class SerializerAccount(serializers.ModelSerializer):
+    status = ModelsStatusSerializer(required=False)
+
     class Meta:
         model = models.ModelsAccount
         fields = '__all__'
