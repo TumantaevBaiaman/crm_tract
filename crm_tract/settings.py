cdfdbfec02a84cbe6d27d21eea17f7dcb8a30147
dinstamaly <dinstamaly@gmail.com>
2023-04-24 13:42:54 +0600

    email password

--- crm_tract/settings.py
+++ crm_tract/settings.py
@@ -185,7 +185,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'aptcrm.service@gmail.com'
-EMAIL_HOST_PASSWORD = 'fgxmzarhsafxzzww'
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
 
 
 
